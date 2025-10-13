672c60dd29705a9b27d58ce4fba287ff6baa1e96
lordtet <lordtet@foxcarries.me>
2025-10-13 19:58:04 -0400

    Added comments, cleaned up fix, set sane default for UID/GID Due to how docker volumes work, chown needed to be in setup.sh. Kind of dirty, but works. UID/GID 1000 was selected as the default for the first Linux user. Common on linux setups with one user, including WSL environments which docker relies on.

--- config/php/setup.sh
+++ config/php/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 composer install
-chown -R www-data:www-data /var/www
 mkdir -p /var/www/html/logs/
+#this needs to go in setup.sh because RUN chown doesn't play nice with volumes
+chown -R www-data:www-data /var/www
 /usr/sbin/apache2ctl -D FOREGROUND
